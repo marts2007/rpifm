3bb4a630a3d3514176ae4c77e98953a1e2c687bc
Umbravir <verepavel@gmail.com>
2017-06-06 18:07:39 +0500

    fix

--- index.php
+++ index.php
@@ -20,7 +20,6 @@ class rpifm {
 	public function __construct($argv){
 		$this->config = require('config.php');
 		$this->curl = new curl();
-		declare(ticks = 1); // enable signal handling
 		if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
 			  //винда :(
 			} else {
@@ -28,6 +27,7 @@ class rpifm {
 			   pcntl_signal(SIGINT, array($this,'saveAllConfigs'));  
 			   pcntl_signal(SIGTERM, array($this,'saveAllConfigs')); 
 			}
+			
 		if (file_exists('vars.php')) {
 			$config = json_decode(file_get_contents('vars.php'));
 			$this->vars = $config;
